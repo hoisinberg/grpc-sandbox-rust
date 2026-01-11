2debc5e09cbc674ea01961cc9116d27c2db697a4
Michael Qian <michaelq94@gmail.com>
2026-01-10 22:05:01 -0800

    RouteGuideService

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::compile_protos("proto/helloworld.proto")?;
+
+    tonic_prost_build::compile_protos("proto/route_guide.proto")
+        .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+
     Ok(())
 }
